467ba06816c5a4110a753980c055f5b522e97499
Momchil Hardalov <momchil.hardalov@gmail.com>
2014-12-23 00:11:53 +0200

    Revert "Added integration and quering with solrj"
    This reverts commit ced9d9d65a47e3c9e393b042b1ed4d377b48a683.

--- src/main/java/org/sports/websearch/contoller/SolrController.java
+++ src/main/java/org/sports/websearch/contoller/SolrController.java
@@ -1,15 +1,5 @@
 package org.sports.websearch.contoller;
 
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrQuery.ORDER;
-import org.apache.solr.client.solrj.SolrQuery.SortClause;
-import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.impl.XMLResponseParser;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.SolrParams;
 import org.sports.websearch.model.Article;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,48 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/solr")
 public class SolrController {
 
-	private SolrServer getSolrServer() {
-		String url = "http://localhost:8983/solr/nutch";
-		HttpSolrServer server = new HttpSolrServer(url);
-		server.setMaxRetries(1); // defaults to 0. > 1 not recommended.
-		server.setConnectionTimeout(5000); // 5 seconds to establish TCP
-		// Setting the XML response parser is only required for cross
-		// version compatibility and only when one side is 1.4.1 or
-		// earlier and the other side is 3.1 or later.
-		server.setParser(new XMLResponseParser()); // binary parser is used by
-													// default
-		// The following settings are provided here for completeness.
-		// They will not normally be required, and should only be used
-		// after consulting javadocs to know whether they are truly required.
-		server.setSoTimeout(1000); // socket read timeout
-		server.setDefaultMaxConnectionsPerHost(100);
-		server.setMaxTotalConnections(100);
-		server.setFollowRedirects(false); // defaults to false
-		// allowCompression defaults to false.
-		// Server side must support gzip or deflate for this to have any effect.
-		server.setAllowCompression(true);
-
-		return server;
-	}
-
 	@RequestMapping(value = "/{query}", method = RequestMethod.GET)
 	public @ResponseBody ResponseEntity<Article> search(
 			@RequestParam(value = "query", required = true) String query) {
 
-		SolrServer server = this.getSolrServer();
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery( query );
-//		solrQuery.addSort(new SortClause("tstamp", ORDER.desc));
-		
-		QueryResponse rsp = null;
-		try {
-			rsp = server.query( solrQuery );
-		} catch (SolrServerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		SolrDocumentList docs = rsp.getResults();
-		
 		System.out.println(query);
 		return null;
 	}
