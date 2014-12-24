807c9b52d55507683f749508b18ba7056d2efb1c
Momchil Hardalov <momchil.hardalov@gmail.com>
2014-12-24 15:40:10 +0200

    added ui router

--- src/main/webapp/public/app.js
+++ src/main/webapp/public/app.js
@@ -1,14 +1,33 @@
-var myApp = angular.module('sportsWebSearch', [ 'ui.router' ]);
+var myApp = angular.module('sportsWebSearch', [ 
+	'SolrSearchController',
+	'ui.router',
+	'ngAnimate' 
+]);
 
-myApp.config(function($stateProvider, $urlRouterProvider) {
+myApp.run(
+		[ '$rootScope', '$state', '$stateParams',
+				function($rootScope, $state, $stateParams) {
+					// It's very handy to add references to $state and
+					// $stateParams to the $rootScope
+					// so that you can access them from any scope within
+					// your applications.For example,
+					// <li ng-class="{ active:
+					// $state.includes('contacts.list') }"> will set the
+					// <li>
+					// to active whenever 'contacts.list' or one of its
+					// decendents is active.
+					$rootScope.$state = $state;
+					$rootScope.$stateParams = $stateParams;
+				} ]).config(function($stateProvider, $urlRouterProvider) {
 	//
-	// For any unmatched url, redirect to /state1
-	$urlRouterProvider.otherwise("dashboard");
+	// For any unmatched url, redirect to /
+	$urlRouterProvider.otherwise("/");
+
 	//
 	// Now set up the states
 	$stateProvider.state('dashboard', {
 		url : "dashboard",
-		templateUrl : "/views/solrSearch.html",
-		controller: "SolrSearchController"
+		template : "<p> somethings here.</p>",
+		controller : "SolrSearchController"
 	})
 });
