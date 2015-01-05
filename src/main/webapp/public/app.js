80d5d1f42f6a24cb534ac59e456cc3ffbb01ea15
Momchil Hardalov <momchil.hardalov@gmail.com>
2015-01-06 00:25:36 +0200

    Prevent default on ng-click <a href .. />

--- src/main/webapp/public/app.js
+++ src/main/webapp/public/app.js
@@ -96,3 +96,16 @@ routerApp.service('anchorSmoothScroll', function(){
 
     };
 });
+
+app.directive('a', function() {
+    return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+            if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
+                elem.on('click', function(e){
+                    e.preventDefault();
+                });
+            }
+        }
+   };
+});
